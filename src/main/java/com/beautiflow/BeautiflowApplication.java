da68b4ba85de7611dcec7ffb2cf93ecd04c54a85
Dongjun <ldj990517@gmail.com>
2025-06-24 18:37:26 +0900

    Initial commit with Spring Boot project

---
+++ src/main/java/com/beautiflow/BeautiflowApplication.java
@@ -0,0 +1,13 @@
+package com.beautiflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautiflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautiflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beautiflow/BeautiflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beautiflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautiflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
