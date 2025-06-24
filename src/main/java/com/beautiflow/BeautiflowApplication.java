71bc579d0d844c423d05967ae3682f5e4244028c
Dongjun <ldj990517@gmail.com>
2025-06-24 19:53:21 +0900

    웹훅 테스트

--- src/main/java/com/beautiflow/BeautiflowApplication.java
+++ src/main/java/com/beautiflow/BeautiflowApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BeautiflowApplication {
 
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(BeautiflowApplication.class, args);
 	}
