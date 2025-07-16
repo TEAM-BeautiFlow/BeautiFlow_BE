8bc227a1fd26a79ed498a1a1f2c0c3c9444ba852
Jeong-Ryeol <jwr5336@naver.com>
2025-07-16 18:28:32 +0900

    [DOCS] Reservation 컨트롤러 수정

--- src/main/java/com/beautiflow/reservation/controller/ReservationController.java
+++ src/main/java/com/beautiflow/reservation/controller/ReservationController.java
@@ -4,22 +4,21 @@ import com.beautiflow.global.common.ApiResponse;
 import com.beautiflow.reservation.dto.ReservationMonthRes;
 import com.beautiflow.reservation.service.ReservationService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/reservations")
 @RequiredArgsConstructor
+@Tag(name = "Reservation", description = "사장님 월별 예약 조회 API")
 public class ReservationController {
 
   private final ReservationService reservationService;
 
-  @GetMapping("/months") // 사장님 예약 월별 조회
+  @GetMapping("/months")
   @Operation(summary = "월별 예약 유무 조회", description = "특정 월에 예약된 날짜별 예약 개수를 조회합니다.")
   public ResponseEntity<ApiResponse<List<ReservationMonthRes>>> getReservedDates(
       @RequestParam Long designerId,
