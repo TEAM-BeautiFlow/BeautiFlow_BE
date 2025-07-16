4363294ec1bb854f4a51091316bfe9165e71c97b
Jeong-Ryeol <jwr5336@naver.com>
2025-07-16 19:02:36 +0900

    [DOCS] #23 ISSUE / 예약 리포지토리에 쿼리 추가

--- src/main/java/com/beautiflow/reservation/repository/ReservationRepository.java
+++ src/main/java/com/beautiflow/reservation/repository/ReservationRepository.java
@@ -2,6 +2,7 @@ package com.beautiflow.reservation.repository;
 
 import com.beautiflow.reservation.domain.Reservation;
 import com.beautiflow.reservation.dto.ReservationMonthRes;
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -18,5 +19,16 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
       @Param("designerId") Long designerId,
       @Param("month") String month
   );
+
+  //시간대별 예약 조회
+  @Query("SELECT r.id, r.customer.name, r.status, r.startTime, r.endTime " +
+      "FROM Reservation r " +
+      "WHERE r.designer.id = :designerId " +
+      "AND r.reservationDate = :date")
+  List<Object[]> findTimeSlotsByDesignerIdAndDate(
+      @Param("designerId") Long designerId,
+      @Param("date") LocalDate date
+  );
+
 
 }
