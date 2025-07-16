4b0841b78e71ef5062c3cbee19c6bbde780f5b02
Jeong-Ryeol <jwr5336@naver.com>
2025-07-16 17:51:03 +0900

    [FEAT] Reservation Controller 추가

---
+++ src/main/java/com/beautiflow/reservation/controller/ReservationController.java
@@ -0,0 +1,32 @@
+package com.beautiflow.reservation.controller;
+
+import com.beautiflow.global.common.ApiResponse;
+import com.beautiflow.reservation.dto.ReservationMonthRes;
+import com.beautiflow.reservation.service.ReservationService;
+import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reservations")
+@RequiredArgsConstructor
+public class ReservationController {
+
+  private final ReservationService reservationService;
+
+  @GetMapping("/months") // 사장님 예약 월별 조회
+  @Operation(summary = "월별 예약 유무 조회", description = "특정 월에 예약된 날짜별 예약 개수를 조회합니다.")
+  public ResponseEntity<ApiResponse<List<ReservationMonthRes>>> getReservedDates(
+      @RequestParam Long designerId,
+      @RequestParam String month
+  ) {
+    List<ReservationMonthRes> result = reservationService.getReservedDates(designerId, month);
+    return ResponseEntity.ok(ApiResponse.success(result));
+  }
+
+}
