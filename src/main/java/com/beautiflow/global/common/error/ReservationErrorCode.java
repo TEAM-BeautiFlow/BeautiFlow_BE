3a6db8c2627e7335003ba168821fa91057c0752d
Jeong-Ryeol <jwr5336@naver.com>
2025-07-16 17:34:23 +0900

    [FEAT] Reservation Errorcode 추가

---
+++ src/main/java/com/beautiflow/global/common/error/ReservationErrorCode.java
@@ -0,0 +1,19 @@
+package com.beautiflow.global.common.error;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ReservationErrorCode implements ErrorCode{
+
+  RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "RESERVATION400", "예약을 찾을 수 없습니다.");
+
+  private final HttpStatus httpStatus;
+  private final String code;
+  private final String message;
+
+
+}
