7db79b6c5eeaf5257d5844d2d95c8710f5fd54a3
Dongjun <ldj990517@gmail.com>
2025-07-17 03:58:41 +0900

    [FEAT] STOMP 및 레디스 설정 추가

--- src/main/java/com/beautiflow/global/common/config/RedisConfig.java
+++ src/main/java/com/beautiflow/global/common/config/RedisConfig.java
@@ -1,15 +1,16 @@
 package com.beautiflow.global.common.config;
 
+
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.springframework.beans.factory.annotation.Qualifier;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 @Configuration
 public class RedisConfig {
+
 
     @Value("${spring.data.redis.host}")
     private String host;
