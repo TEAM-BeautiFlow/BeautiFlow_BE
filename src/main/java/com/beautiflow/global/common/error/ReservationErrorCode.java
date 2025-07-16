ca28d8a6694f35914477d2d99b3d502311d764db
Jeong-Ryeol <jwr5336@naver.com>
2025-07-16 21:07:44 +0900

    [DOCS] ISSUE#25 에러코드 구문 추가

--- src/main/java/com/beautiflow/global/common/error/ReservationErrorCode.java
+++ src/main/java/com/beautiflow/global/common/error/ReservationErrorCode.java
@@ -10,7 +10,9 @@ import lombok.RequiredArgsConstructor;
 public enum ReservationErrorCode implements ErrorCode{
 
   RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "RESERVATION400", "예약을 찾을 수 없습니다."),
-  RESERVATION_TIME_NOT_FOUND(HttpStatus.NOT_FOUND, "RESERVATION401", "지정된 날짜의 예약을 찾을 수 없습니다.");
+  RESERVATION_TIME_NOT_FOUND(HttpStatus.NOT_FOUND, "RESERVATION401", "지정된 날짜의 예약을 찾을 수 없습니다."),
+  RESERVATION_DETAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "RESERVATION402", "세부내역을 찾을 수 없습니다."),
+  RESERVATION_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "RESERVATION403", "예약상태를 확인할 수 없습니다.");
 
   private final HttpStatus httpStatus;
   private final String code;
