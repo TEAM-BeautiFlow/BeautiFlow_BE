4f79cbff59b6b78f1f3a3b91732d93e9464b27d2
Dongjun <ldj990517@gmail.com>
2025-07-20 15:51:58 +0900

    [FEAT] TEMPLATE인 알림인 경우 실시간 전송 로직 추가

--- src/main/java/com/beautiflow/global/common/Alert/AlertEventHandler.java
+++ src/main/java/com/beautiflow/global/common/Alert/AlertEventHandler.java
@@ -8,10 +8,15 @@ import org.springframework.transaction.annotation.Transactional;
 import com.beautiflow.chat.dto.ChatMessageSendReq;
 import com.beautiflow.chat.repository.ChatRoomReadRepository;
 import com.beautiflow.chat.service.ChatMessageService;
+import com.beautiflow.chat.service.RedisPubSubService;
 import com.beautiflow.global.common.sms.SmsService;
 import com.beautiflow.global.domain.SenderType;
 import com.beautiflow.user.domain.User;
 import com.beautiflow.user.repository.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,6 +28,12 @@ public class AlertEventHandler {
 	private final ChatMessageService chatMessageService;
 	private final ChatRoomReadRepository chatRoomReadRepository;
 	private final UserRepository userRepository;
+	private final RedisPubSubService pubSubService;
+
+	private final ObjectMapper objectMapper = new ObjectMapper()
+		.registerModule(new JavaTimeModule())
+		.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
 
 	@Transactional
 	public void handle(AlertEvent event) {
@@ -32,13 +43,21 @@ public class AlertEventHandler {
 		if (event.getType().equals("SMS")) {
 			smsService.sendReminderTo(receiver.getContact());
 		} else if (event.getType().equals("TEMPLATE")) {
-			chatMessageService.saveMessage(event.getChatRoomId(), new ChatMessageSendReq(
+			ChatMessageSendReq sendReq = new ChatMessageSendReq(
 				event.getChatRoomId(),
 				event.getSenderId(),
 				SenderType.STAFF,
 				event.getMessageContent(),
 				null
-			));
+			);
+			chatMessageService.saveMessage(event.getChatRoomId(), sendReq);
+
+			try {
+				String msg = objectMapper.writeValueAsString(sendReq);
+				pubSubService.publish("chat", msg);
+			} catch (JsonProcessingException e) {
+				throw new RuntimeException("Failed to serialize chat message", e);
+			}
 		}
 
 		chatRoomReadRepository.findByChatRoomIdAndUserId(event.getChatRoomId(), event.getReceiverId())
