cbb6a0c5862d32c41a6e8d91f4cc5bf546172e5f
Dongjun <ldj990517@gmail.com>
2025-07-20 15:42:27 +0900

    [FEAT] 레디스컨피그 설정 추가

--- src/main/java/com/beautiflow/global/common/config/RedisConfig.java
+++ src/main/java/com/beautiflow/global/common/config/RedisConfig.java
@@ -8,6 +8,7 @@ import org.redisson.config.Config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class RedisConfig {
 
--- src/main/java/com/beautiflow/global/domain/ReservationStatus.java
+++ src/main/java/com/beautiflow/global/domain/ReservationStatus.java
@@ -1,11 +1,10 @@
 package com.beautiflow.global.domain;
 
 public enum ReservationStatus {
-	TEMPORARY, // 임시 예약
-	PENDING,       // 확정대기
-	CONFIRMED,     // 예약 확정
-	CANCELLED,     // 취소
-	NO_SHOW,       // 노쇼
-	COMPLETED      // 시술 완료
+    TEMPORARY, // 임시 예약
+    PENDING,       // 확정대기
+    CONFIRMED,     // 예약 확정
+    CANCELLED,     // 취소
+    NO_SHOW,       // 노쇼
+    COMPLETED      // 시술 완료
 }
->>>>>>> 9e32b7f ([REFACT] ReservationStatus 값 추가)
