9a411465c1f96e83ddb12ab5cc57a9edc849c2b0
bbandm <hjerachel@gmail.com>
2025-08-12 16:39:47 +0900

    [FIX] reservationRepository conflict 해결 3

--- src/main/java/com/beautiflow/reservation/repository/ReservationRepository.java
+++ src/main/java/com/beautiflow/reservation/repository/ReservationRepository.java
@@ -99,13 +99,4 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
             @Param("targetDate") LocalDate targetDate,
             @Param("cutoff") LocalTime cutoff
     );
-
-    @Query("""
-    SELECT DISTINCT r FROM Reservation r
-    LEFT JOIN FETCH r.reservationOptions ro
-    LEFT JOIN FETCH ro.optionGroup
-    WHERE r.status = :status
-""")
-    List<Reservation> findAllByStatusWithOptionsAndGroups(@Param("status") ReservationStatus status);
-
 }
