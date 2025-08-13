f90b7730b3ac367baefb2746d40ee81280b2c3d2
Jeong-Ryeol <jwr5336@naver.com>
2025-08-14 04:04:31 +0900

    [DOCS] 사용하지 않는 import문 수정

--- src/main/java/com/beautiflow/ManagedCustomer/dto/CustomerListSimpleRes.java
+++ src/main/java/com/beautiflow/ManagedCustomer/dto/CustomerListSimpleRes.java
@@ -6,13 +6,13 @@ import java.util.List;
 public record CustomerListSimpleRes(
     Long customerId,
     String name,
-    List<String> groupCodes // 그룹 코드 리스트
+    List<String> groupCodes
 ) {
   public static CustomerListSimpleRes from(ManagedCustomer mc) {
     List<String> codes = mc.getGroups().isEmpty()
         ? List.of()
         : mc.getGroups().stream()
-            .map(g -> g.getCode()) // code만 추출
+            .map(g -> g.getCode())
             .toList();
 
     return new CustomerListSimpleRes(
